36d43491d28f52177f7f98058a5308d718257b68
Steven Op de beeck <steven.opdebeeck@sfpd.fgov.be>
2025-06-17 07:42:00 +0200

    add windows script

---
+++ performance-test.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set GRADLE_DIR=%CD%\gradle_user_home
+
+REM Create test directory if it doesn't exist
+if not exist "%GRADLE_DIR%" mkdir "%GRADLE_DIR%"
+
+set GRADLE_USER_HOME=%GRADLE_DIR%
+
+echo === DEPENDENCY SETUP ===
+gradlew.bat build --refresh-dependencies -q --continue -x :spring-webflux:test -x checkstyleNohttp
+gradlew.bat clean -q
+
+echo === PERFORMANCE TESTING ===
+echo Start time: %time%
+gradlew.bat clean build -x :spring-webflux:test -x checkstyleNohttp --rerun-tasks --offline --parallel
+echo End time: %time%
